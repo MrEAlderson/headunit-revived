4b5d41ea69e7dd0f8be4c0f9dd2a38dce87a12b2
Marcel <8513034+MrEAlderson@users.noreply.github.com>
2026-07-13 00:08:37 +0200

    Removed debug

--- app/src/main/java/com/andrerinas/headunitrevived/connection/carkey/fyt/CarFYTReceiver.kt
+++ app/src/main/java/com/andrerinas/headunitrevived/connection/carkey/fyt/CarFYTReceiver.kt
@@ -116,159 +116,8 @@ class CarFYTReceiver : CarKeyReceiver {
 
             this.toolkit = RemoteToolkit.Stub.asInterface(binder)
 
-            // com.syu.steer.module.MySteer
-            //this.module = toolkit!!.getRemoteModule(0)!!
-            //val callback = AAPCallback(context)
-
-            /*module!!.register(callback, 0, 0)
-            module!!.register(callback, 2, 0)
-            module!!.register(callback, 3, 0)
-            module!!.register(callback, 4, 1)
-            module!!.register(callback, 5, 0)
-            module!!.register(callback, 7, 0)
-            module!!.register(callback, 8, 0)
-            module!!.register(callback, 9, 0)
-            module!!.register(callback, 10, 0)
-            module!!.register(callback, 6, 0)*/
-            //for (i in 0 until 20)
-            //    module!!.register(callback, i, 1)
-
-            // CarLinkService f1203S, f1204T, f1205U
-            //observe(0, intArrayOf(0, 1, 50, 43, 4, 138, 133, 60, 166, 167, 168))
-            //observe(2, intArrayOf(6, 7, 15, 14, 31, 9))
-            //observe(7, intArrayOf(1003))
             observe(0, intArrayOf(133))
-
-            /*val systemBind: IBinder = SystemService.get("CarplayServer")
-            //carplayServer.linkToDeath(this, 0)
-            val parcelObtain = Parcel.obtain()
-            val parcelObtain2 = Parcel.obtain()
-            val coolBinder = CoolBinder(context, modules)
-            var registerResult: Boolean = false
-
-            try {
-                parcelObtain.writeInterfaceToken("CarplayServer.ICarplayService")
-                parcelObtain.writeStrongBinder(coolBinder)
-                systemBind.transact(3, parcelObtain, parcelObtain2, 0)
-                registerResult = parcelObtain2.readInt() != 0
-                parcelObtain2.readException()
-            } finally {
-                parcelObtain2.recycle();
-                parcelObtain.recycle();
-            }
-
-            Log.i("MEOW", "REGISTER RESULT: $registerResult")
-            Handler(Looper.getMainLooper()).post {
-                Toast.makeText(
-                    context,
-                    """F
-            REGISTER RESULT=$registerResult
-            """.trimIndent(),
-                    Toast.LENGTH_SHORT,
-                ).show()
-            }*/
-
-
-            /// xx
-            /*fun transactSystem(code: Int, data: Parcel, reply: Parcel): Boolean {
-                return systemBind.transact((code shl 8) or 2, data, reply, 0)
-            }
-
-            fun complexTransactSystem(code: Int, ints: IntArray, strings: Array<String>?): Boolean {
-                val data = Parcel.obtain()
-                val reply = Parcel.obtain()
-
-                try {
-                    data.writeInterfaceToken("CarplayServer.ICarplayService")
-
-                    for (i in ints)
-                        data.writeInt(i)
-
-                    if (strings != null) {
-                        for (s in strings)
-                            data.writeString(s)
-                    }
-
-                    return transactSystem(code, data, reply) && reply.readInt() >= 0
-                } finally {
-                    data.recycle()
-                    reply.recycle()
-                }
-            }
-
-            /// XXXXX
-            var data = Parcel.obtain()
-            var reply = Parcel.obtain()
-            var regCode: Int
-            var r9: Int = 4
-
-
-            try {
-                data.writeInterfaceToken("CarplayServer.ICarplayService")
-
-                if (!transactSystem(215, data, reply))
-                    Log.i("MEOW", "FUCK")
-
-                regCode = reply.readInt()
-            } finally {
-                data.recycle()
-                reply.recycle()
-            }
-
-
-            Log.i("MEOW", "REG CODE: $regCode")*/
-
-            /*if (regCode == 0)
-                r9 = 3
-            else if (regCode == 2)
-                r9 = 6
-            else if (regCode == 3)
-                r9 = 7
-            else if (regCode == 4)
-                r9 = 5
-            else
-                r9 = 3*/
-
-
-            //// XXXXX
-            /*data = Parcel.obtain()
-            reply = Parcel.obtain()
-
-            try {
-                // complexTransactSystem 218
-
-                data.writeInterfaceToken("CarplayServer.ICarplayService")
-                data.writeString(null /* SystemProperties.get("sys.syu.carlink.logo_label") */)
-                data.writeString("/sdcard/carlink/logo/car_log.png")
-                data.writeInt(180)
-                data.writeInt(180)
-
-                if (!transactSystem(213, data, reply))
-                    Log.i("MEOW", "213 failed?!")
-                reply.readInt()
-            } finally {
-                data.recycle()
-                reply.recycle()
-
-                complexTransactSystem(214, intArrayOf(1), null) // driveMode ?
-                complexTransactSystem(219, intArrayOf(0), null) // like that already
-                complexTransactSystem(223, intArrayOf(1), null) // audio media ?
-            }*/
-
-            //modules[0]!!.cmd(0, intArrayOf(10))
-            //modules[0]!!.cmd(133, intArrayOf(3), null, null)
-            //modules[0]!!.cmd(182, intArrayOf(3, 4), null, null)
-
             suExecutor.setProp("sys.carlink.type", "2")
-            Log.i("MEOW", ":) OK START")
-
-
-
-            // registeres modules 0-20, then calls #register updateCode 0-1, update:0 ??
-
-            // com.syu.steer.ipc.Ipc_New
-            // start receiving updates
-            //module!!.cmd(2, ints = intArrayOf(1))
         }
 
         fun observe(moduleCode: Int, codes: IntArray) {
@@ -294,110 +143,21 @@ class CarFYTReceiver : CarKeyReceiver {
         private inner class AAPCallback(val moduleCode: Int) :
             ModuleCallback.Stub() {
 
-            var rAppId: Int? = null
-            var rRequest: Int? = null
-
             override fun update(
                 updateCode: Int,
                 ints: IntArray?,
                 floats: FloatArray?,
                 strings: Array<String>?,
             ) {
-                Handler(Looper.getMainLooper()).post {
-                    Toast.makeText(
-                        context,
-                        """A
-            moduleCode=$moduleCode
-            code=$updateCode
-            ints=${ints?.joinToString() ?: "null"}
-            floats=${floats?.joinToString() ?: "null"}
-            strings=${strings?.joinToString() ?: "null"}
-            """.trimIndent(),
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                }
-// link.stateType == 4 && link.value == 10
-// ServiceConnection.m1029c(CarLinkService.this.getServiceConnection(), 0, 133, new int[]{3}, null, null, 24);
-                // CarLinkService#onHandle
-                /*if (moduleCode == 0 && updateCode == 133) {
-                    ints?.size?.let {
-                        if (it >= 1)
-                            ints[0]
-                    }
-                }*/
 
                 if (moduleCode == 0) {
                     when (updateCode) {
-                        /*0 -> {
-                            if (ints == null || ints.isEmpty())
-                                return
-
-                            val id = ints[0]
-
-                            if (rRequest == id) {
-                                Log.i("MEOW", "Request appid $id finish!!")
-                                rRequest = null
-                                rAppId = id
-
-                            } else if (rRequest == null) {
-                                if (rAppId != id) {
-                                    Log.i("MEOW", "Exit appid: $id")
-                                    rAppId = id
-
-                                    // ...
-                                }
-
-                                // ..
-                            } else {
-                                if (rAppId == rRequest)
-                                    return
-                                Log.i("MEOW", "Request appid: $rRequest")
-
-                                this.rRequest = rAppId
-                                // ..
-                            }
-                        }
-
-                        1 -> { // mcu state
-                            if (ints == null || ints.isEmpty())
-                                return
-
-                            val mcuOn = ints[0] == 1
-                            Log.i("MEOW", "MCU On: $mcuOn")
-
-                            /*if (CarLink.INSTANCE!!.z() != mcuOn) {
-                                this.i = 0
-                                CarLink.INSTANCE!!.liveDatas.d.postValue(mcuOn)
-                            }*/
-                        }
-
-                        4 -> {
-                            if (ints == null || ints.isEmpty())
-                                return
-
-                            /*
-                                            if (msg.c.isNullOrEmpty()) return
-                val isNightMode = msg.c[0] == 1
-                if (isNightMode != CarLink.INSTANCE!!.x()) {
-                    CarLink.INSTANCE!!.liveDatas.h.postValue(isNightMode)
-                }
-                             */
-                        }
-
-                        43 -> {
-                            if (ints == null || ints.isEmpty())
-                                return
-                            /*
-
-                             */
-                        }*/
-
                         133 -> {
                             if (ints?.size == 0)
                                 return
 
                             val key = ints!![0]
-                            Log.i("CarFYT", "Clicked key $key")
+                            Log.i("CarKeyReceiver", "Clicked key $key")
 
                             // assistant
                             if (key == 576) {
@@ -409,37 +169,6 @@ class CarFYTReceiver : CarKeyReceiver {
                         }
                     }
                 }
-            }
-        }
-
-        private class CoolBinder(val context: Context, val modules: Map<Int, RemoteModule>) : Binder() {
-
-            override fun onTransact(code: Int, data: Parcel, reply: Parcel?, flags: Int): Boolean {
-                val crypticCode: Int = code shr 8
-
-                Handler(Looper.getMainLooper()).post {
-                    Toast.makeText(
-                        context,
-                        """B
-            crypticCode=$crypticCode
-            code=$code
-            """.trimIndent(),
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                }
-
-                if ((code and 255) != 1)
-                    return super.onTransact(code, data, reply, flags)
-                if (crypticCode == 100 || crypticCode == 101)
-                    return super.onTransact(code, data, reply, flags)
-
-                modules[0]!!.cmd(133, intArrayOf(3), null, null)
-
-                if (reply == null)
-                    return true
-
-                reply.writeInt(0)
-                return true
             }
         }
     }
